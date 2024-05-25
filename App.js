27165e3f9e6e0b8d20cf3f55c0cad3eb43d816ea
NARENDRA9959 <cnarendra329@gmail.com>
2024-05-25 11:27:01 +0530

    DONE

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', {}, 'Welcome to react workshop');
+const heading = <div>
+    <h1>Welcome to Simuleduco Solutions</h1>
+    <span>Welcome to React Workshop</span>
+</div>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
