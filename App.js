e1d1ed49c736b00fe8046526d90e1c8c76d9fa48
NARENDRA9959 <cnarendra329@gmail.com>
2024-06-02 16:55:09 +0530

    Done

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = <div>
-    <h1>Welcome to Simuleduco Solutions</h1>
-    <span>Welcome to React Workshop</span>
-</div>;
+const jsxSpanElement = <h1 className='heading'>Welcome to React Course</h1>;
+
+console.log("jsxSpanElement", jsxSpanElement);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(jsxSpanElement);
